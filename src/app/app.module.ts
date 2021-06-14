65dce68716575f9d1ecced97d2fd99882701f7e8
VietAnh.Hoang <vietanh123456123@gmail.com>
2021-06-14 14:34:46 +0700

    sadasd

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
